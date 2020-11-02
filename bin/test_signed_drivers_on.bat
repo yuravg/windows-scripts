bc8bd48c93064487296cb15d1f5ee8a3b7ef4ec2
Yuriy VG <yuravg@gmail.com>
2020-11-02 12:41:57 +0300

    Improve stdout message for develop-mode scripts.

--- bin/test_signed_drivers_on.bat
+++ bin/test_signed_drivers_on.bat
@@ -1,3 +1,4 @@
+@echo off
 rem
 rem Enable Loading of Test Signed Drivers
 rem
@@ -6,7 +7,12 @@ rem
 rem Administrator rights required:
 rem  Press Windows+R to open the "Run" box. Type "cmd" into the box and then press
 rem  Ctrl+Shift+Enter to run the command as an administrator.
-rem
-@echo off
+
+echo ***** ENABLE Loading of Test Signed Drivers *****
+echo NOTE: Administrator rights required!
+echo        (press Windows+R to open the "Run" box;
+echo         Ctrl+Shift+Enter to run the command as an administrator)
+@echo on
+
 bcdedit /set testsigning on
 pause
