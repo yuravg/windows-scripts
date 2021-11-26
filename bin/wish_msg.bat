a6350f2cacc08388b2d27c46cd36b499771b4c82
Yuriy VG <yuravg@gmail.com>
2021-11-26 09:17:37 +0300

    Add script to run wish message window
    run 'wish_cmd' script from:
    https://github.com/yuravg/eda-scripts/blob/master/bin/wish_cmd

---
+++ bin/wish_msg.bat
@@ -0,0 +1,7 @@
+@echo off
+rem
+rem Run: wish(Tcl/Tk Simple windowing shell) message window
+rem      https://github.com/yuravg/eda-scripts/blob/master/bin/wish_cmd
+rem
+@echo on
+wish %HOME%\projects\eda-scripts\bin\wish_msg %*
