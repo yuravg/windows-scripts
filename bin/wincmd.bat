454f2ad792100fa115d33c6f64d216703bab3ff8
Yuriy VG <yuravg@gmail.com>
2025-11-20 16:02:59 +0300

    Add command to print reference about main Windows commands.

---
+++ bin/wincmd.bat
@@ -0,0 +1,43 @@
+@echo off
+echo.
+echo Windows Run Commands Reference
+echo ================================
+echo.
+echo "."            - Open current user folder.
+echo appwiz.cpl     - Programs and Features.
+echo charmap        - Windows Character Map.
+echo chkdsk         - Check Disk.
+echo cleanmgr       - Disk Cleanup.
+echo cmd            - Command Prompt.
+echo compmgmt.msc   - Computer Management.
+echo control        - Control Panel.
+echo controlfolders - Folder Options.
+echo devmgmt.msc    - Device Manager.
+echo diskmgmt.msc   - Disk Management.
+echo dxdiag         - DirectX Diagnostic Tool.
+echo eventvwr.msc   - Event Viewer.
+echo explorer       - Windows Explorer.
+echo gpedit.msc     - Local Group Policy Editor.
+echo magnify        - Magnifier.
+echo main.cpl       - Mouse Properties.
+echo mdsched        - Windows Memory Diagnostic.
+echo mrt            - Microsoft Windows Malicious Software Removal Tool.
+echo msconfig       - System Configuration.
+echo msinfo32       - System Information.
+echo mstsc          - Remote Desktop Connection.
+echo ncpa.cpl       - Network Connections.
+echo netplwiz       - User Accounts.
+echo osk            - On-Screen Keyboard.
+echo powercfg.cpl   - Power Options.
+echo powershell     - Windows PowerShell.
+echo regedit        - Registry Editor.
+echo resmon         - Resource Monitor.
+echo secpol.msc     - Local Security Policy.
+echo services.msc   - Services.
+echo shutdown       - Shutdown computer.
+echo snippingtool   - Snipping Tool.
+echo sysdm.cpl      - System Properties / Environment Variables.
+echo taskmgr        - Task Manager.
+echo winver         - Windows Version.
+echo.
+pause
